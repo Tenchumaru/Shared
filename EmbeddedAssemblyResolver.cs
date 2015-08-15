493d25bdddae4702cb3f01ed7013c1afb19d1831
Tenchumaru <Tenchumaru@users.noreply.github.com>
2015-08-15 09:02:23 -0700

    Initial commit

---
+++ EmbeddedAssemblyResolver.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+static class EmbeddedAssemblyResolver
+{
+	internal static void HookResolve(params string[] namespaceNames)
+	{
+#if !DEBUG
+		var assembly = Assembly.GetEntryAssembly();
+		string namespaceName = namespaceNames.FirstOrDefault() ?? assembly.EntryPoint.DeclaringType.Namespace;
+		AppDomain.CurrentDomain.AssemblyResolve += (sender, args) =>
+		{
+			string[] parts = args.Name.Split(',');
+			string name = parts[0];
+			string culture = parts[2];
+			if(name.EndsWith(".resources") && !culture.EndsWith("neutral"))
+				return null;
+			var assemblyName = new AssemblyName(args.Name);
+			string resourceName = namespaceName + '.' + assemblyName.Name + ".dll";
+			using(var stream = assembly.GetManifestResourceStream(resourceName))
+			{
+				var assemblyData = new byte[stream.Length];
+				stream.Read(assemblyData, 0, assemblyData.Length);
+				return Assembly.Load(assemblyData);
+			}
+		};
+#endif
+	}
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Shared Files
+
+Other projects reference these files.  The .NET projects reference
+`Adrezdi.snk` and `EmbeddedAssemblyResolver.cs`.  The C++ projects
+reference `commctrl.xml`, `finger.rc`, and `resource.h`. Both types of
+projects reference `finger.ico`.
